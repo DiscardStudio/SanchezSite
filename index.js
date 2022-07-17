948be29005bf013656cb12667fd3ebc85979ab12
Mike <msanchezmbm@gmail.com>
2022-07-17 17:54:33 -0400

    Still sippin

--- index.js
+++ index.js
@@ -67,6 +67,8 @@ const port = process.env.PORT || 5000;
 
 app.use(express.static(path.resolve(__dirname, './front/build')));
 
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, './front/build', 'index.html'));
 });
