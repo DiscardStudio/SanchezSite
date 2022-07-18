58aa930b4f46c4285402f277115dbc03147d6be9
Michael Sanchez <msanchezmbm@gmail.com>
2022-07-18 17:37:16 -0400

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- index.js
+++ index.js
@@ -77,7 +77,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/signup', (req, res) => {
-    pool.query(`select email from auth where email='${req.query.email}'`, 
+    pool.query(`select email from auth where email="${req.query.email}"`, 
         (err, result) => {
         if(err) {
             res.json({status:404});
