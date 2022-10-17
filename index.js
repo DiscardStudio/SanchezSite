bae666f45bb23202c4b8d2277ca7b0d6679633b9
Mike <secti0n33@live.com>
2022-10-17 11:31:21 -0400

    Made additions to site to include Discord Bot

--- index.js
+++ index.js
@@ -32,3 +32,48 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
+
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({
+	intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions],
+    partials: [Partials.Message, Partials.Channel, Partials.Reaction],
+});
+
+client.once('ready', () => {
+	console.log('Bot is Ready!');
+});
+
+client.on('messageReactionAdd', async (reaction, user) => {
+    // When a reaction is received, check if the structure is partial
+    const x = new Map([
+        ["🌵", 0],
+        ["🤢", 1],
+        ["😷", 2,] ["🧀", 3], 
+        ["😭", 4]
+    ]);
+	if (reaction.partial) {
+		// If the message this reaction belongs to was removed, the fetching might result in an API error which should be handled
+		try {
+			await reaction.fetch();
+		} catch (error) {
+			console.error('Something went wrong when fetching the message:', error);
+			// Return as `reaction.message.author` may be undefined/null
+			return;
+        }
+        
+        user.roles.addRole(
+          client.channels.get(992805236508999760).guild.roles.find(
+            role => role.name ===  ("STEP " + (x.get(reaction.emoji.name) + 19))
+            )
+          );
+	}
+
+	// Now the message has been cached and is fully available
+	console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
+	// The reaction is now also fully available and the properties will be reflected accurately:
+	console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
+});
+
+client.login(process.env.TOKEN);
